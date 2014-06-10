80f0f166d514a618321d932f3fbccc8c4f32b9a1
nadinejamila <nadinejamila@yahoo.com>
2014-06-10 17:03:00 +0800

    db

--- models/DetachmentContactPersonsDB.py
+++ models/DetachmentContactPersonsDB.py
@@ -2,15 +2,6 @@
 import db, import_file
 DetachmentContactPersons = import_file.import_file('DetachmentContactPersons')
 
-def getDetachmentContactPersons():
-  res = db.List("DetachmentContactPersons")
-  DetachmentContactPersonsList = []
-  for row in res:
-    if row is not None:
-      DetachmentContactPerson = DetachmentContactPersons.DetachmentContactPersons( int(row[0]), str(row[1]), str(row[2]), str(row[3]), str(row[4]), str(row[5]) )
-      DetachmentContactPersonsList.append(DetachmentContactPerson)
-      row = cur.fetchone()
-  return ClientContactPersonsList
 
 def getDetachmentContactPersons(val):
   res = db.SubList("DetachmentContactPersons", "DetachID", val)
@@ -19,5 +10,5 @@ def getDetachmentContactPersons(val):
     if row is not None:
       DetachmentContactPerson = DetachmentContactPersons.DetachmentContactPersons( int(row[0]), int(row[1]), str(row[2]), str(row[3]), str(row[4]), str(row[5]), str(row[6]), str(row[7]), str(row[8]) )
       DetachmentContactPersonsList.append(DetachmentContactPerson)
-      row = cur.fetchone()
+      row = db.cur.fetchone()
   return DetachmentContactPersonsList
